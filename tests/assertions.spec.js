b6eae9b2795fa0f723166018407e482e8cee36a9
gaurang4u <welcomegaurang@gmail.com>
2026-03-25 10:37:03 +0530

    Assertions

---
+++ tests/assertions.spec.js
@@ -0,0 +1,27 @@
+import test, { page, expect } from '@playwright/test'
+
+test('Assertion Demo', async ({ page }) => {
+
+    await page.goto('https://kitchen.applitools.com/')
+    await page.pause()
+
+    await expect(page.locator('text=The Kitchen')).toHaveCount(1)
+    if (await page.$('text=The Kitchen')) {
+        await page.locator('text=The Kitchen').click()
+    }
+
+    await expect(page.locator('text=The Kitchen')).toBeVisible()
+    //await expect.soft(page.locator('text=The Kitchen')).toBeHidden()
+    await expect(page.locator('text=The Kitchen')).toBeEnabled()
+    //await expect.soft(page.locator('text=The Kitchen')).toBeDisabled()
+    await expect(page.locator('text=The Kitchen')).toHaveText('The Kitchen')
+    //await expect(page.locator('text=The Kitchen')).not.toHaveText('The Kitchen')
+    await expect(page.locator('text=The Kitchen')).toHaveAttribute('class', /.*css-dpmy2a/)
+    await expect(page.locator('text=The Kitchen')).toHaveClass(/.*css-dpmy2a/)
+    await expect(page).toHaveURL('https://kitchen.applitools.com/')
+    await expect(page).toHaveTitle(/.*Kitchen/)
+     await page.pause()
+    await expect(page).toHaveScreenshot()
+
+
+})
